f1c13e78f33d1f13d2a7adf1f96ec0b0905b3c4e
xieno1 <xdehuan@163.com>
2018-10-11 16:09:17 +0800

    update contract sdk: sdk-contract-1.1.2.jar

--- src/io/nuls/contract/idea/plugin/NulsConstants.java
+++ src/io/nuls/contract/idea/plugin/NulsConstants.java
@@ -2,6 +2,6 @@ package io.nuls.contract.idea.plugin;
 
 public class NulsConstants {
 
-    public static final String SDK = "sdk-contract-1.1.1.jar";
+    public static final String SDK = "sdk-contract-1.1.2.jar";
 
 }
